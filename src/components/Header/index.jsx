3c36414bf4357641b6fb6ad78949d523c201b66c
Dario Reis <guttyerresjunior1995@gmail.com>
2024-04-03 05:34:25 -0300

    Feat: Installing the react-icons icon library and creating and styling the exit button

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,4 +1,6 @@
-import { Container, Profile } from "./styles";
+import { RiShutDownLine } from "react-icons/ri";
+
+import { Container, Profile, Logout } from "./styles";
 
 export function Header() {
   return (
@@ -11,6 +13,10 @@ export function Header() {
           <strong>Dario Reis</strong>
         </div>
       </Profile>
+
+      <Logout>
+        <RiShutDownLine />
+      </Logout>
     </Container>
   );
 }
