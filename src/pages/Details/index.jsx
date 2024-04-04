05b908c43d77d5d434fafe30d1570cdc71921af1
Dario Reis <guttyerresjunior1995@gmail.com>
2024-04-04 06:30:51 -0300

    Feat: Create tag section, configure and style

--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -3,6 +3,7 @@ import { Container, Links } from "./styles";
 import { Button } from "../../components/Button";
 import { Header } from "../../components/Header";
 import { Section } from "../../components/Section";
+import { Tag } from "../../components/Tag";
 
 export function Details() {
   return (
@@ -15,6 +16,12 @@ export function Details() {
           <li><a href="#">https://darioreisjr.vercel.app/</a></li>
         </Links>
       </Section>
+
+      <Section title="Marcadores">
+        <Tag title="express"/>
+        <Tag title="nodejs"/>
+      </Section>
+
 
       <Button title="Voltar" />
     </Container>
