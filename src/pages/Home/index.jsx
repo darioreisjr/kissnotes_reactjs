a52d3d58cb5cf1653abb39a4a7008f49b5c48d6e
Dario Reis <guttyerresjunior1995@gmail.com>
2024-04-08 04:56:26 -0300

    Feat: creating a home page, applying Grid to the Home and marking the page with a grid

---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,23 @@
+import { Container, Brand, Menu, Search, Content, NewNote } from "./styles";
+
+import { Header } from "../../components/Header";
+
+export function Home() {
+  return (
+    <Container>
+      <Brand>
+        <h1>Kissnotes</h1>
+      </Brand>
+
+      <Header/>
+
+      <Menu></Menu>
+
+      <Search></Search>
+
+      <Content></Content>
+
+      <NewNote></NewNote>
+    </Container>
+  );
+}
