32841242aec6a506119a94165f3db5468a60e5b0
Dario Reis <guttyerresjunior1995@gmail.com>
2024-04-10 05:07:30 -0300

    Feat: Creating, styling and formatting signin page form

--- src/pages/Signin/index.jsx
+++ src/pages/Signin/index.jsx
@@ -1,9 +1,27 @@
-import {Container} from "./styles";
+import { FiMail, FiLock } from "react-icons/fi";
+
+import { Input } from "../../components/Input";
+import { Button } from "../../components/Button";
+
+import { Container, Form } from "./styles";
 
 export function Signin() {
   return (
     <Container>
-      <h1>Signin</h1>
+      <Form>
+        <h1>Kiss Notes</h1>
+        <p>Aplicação para salvar e gerenciar seus links últeis.</p>
+
+        <h2>Faça seu login</h2>
+
+        <Input placeholder="E-mail" type="text" icon={FiMail} />
+
+        <Input placeholder="Senha" type="password" icon={FiLock} />
+
+        <Button title="Entrar" />
+
+        <a href="#">Criar conta</a>
+      </Form>
     </Container>
   );
 }
