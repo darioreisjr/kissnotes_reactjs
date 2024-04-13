f59aeaf18e1987a50508f8eebad677f5485d6924
Dario Reis <guttyerresjunior1995@gmail.com>
2024-04-13 17:52:23 -0300

    Feat: creating new tag session and structuring and styling along with save button

--- src/pages/New/index.jsx
+++ src/pages/New/index.jsx
@@ -3,6 +3,7 @@ import { Input } from "../../components/Input";
 import { TextArea } from "../../components/TextArea";
 import { Section } from "../../components/Section";
 import { NoteItem } from "../../components/NoteItem";
+import { Button } from "../../components/Button";
 
 import { Container, Form } from "./styles";
 
@@ -25,6 +26,15 @@ export function New() {
             <NoteItem value="https://darioreisjr.vercel.app/" />
             <NoteItem isNew placeholder="Novo link" />
           </Section>
+
+          <Section title="Marcadores">
+            <div className="tags">
+              <NoteItem value="React" />
+              <NoteItem isNew placeholder="Novo Tag" />
+            </div>
+          </Section>
+
+          <Button title="Salvar" />
         </Form>
       </main>
     </Container>
