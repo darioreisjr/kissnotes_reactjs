b0cf5b2e69d44272ce76115750e52d5b345804b6
Dario Reis <guttyerresjunior1995@gmail.com>
2024-05-16 05:20:11 -0300

    Feat:Applying the functionality to return to the details page and improving navigation

--- src/pages/New/index.jsx
+++ src/pages/New/index.jsx
@@ -1,5 +1,4 @@
 import { useState } from "react";
-import { Link } from "react-router-dom";
 
 import { useNavigate } from "react-router-dom";
 
@@ -9,6 +8,7 @@ import { TextArea } from "../../components/TextArea";
 import { Section } from "../../components/Section";
 import { NoteItem } from "../../components/NoteItem";
 import { Button } from "../../components/Button";
+import { ButtonText } from "../../components/ButtonText";
 
 import { api } from "../../services/api";
 
@@ -25,6 +25,10 @@ export function New() {
   const [newTag, setNewTag] = useState("");
 
   const navigate = useNavigate();
+
+  function handleBack() {
+    navigate(-1);
+  }
 
   function handleAddLink() {
     setLinks((prevState) => [...prevState, newLink]);
@@ -69,7 +73,7 @@ export function New() {
         links,
       });
       alert("Nota criada com sucesso!");
-      navigate("/");
+      navigate(-1);
     } catch (error) {
       console.error("Error creating note:", error);
       alert("Houve um erro ao criar a nota. Tente novamente mais tarde."); // Inform user about error
@@ -84,7 +88,7 @@ export function New() {
         <Form>
           <header>
             <h1>Criar nota</h1>
-            <Link to="/">Voltar</Link>
+            <ButtonText title="Voltar" onClick={handleBack} />
           </header>
 
           <Input
--- src/pages/Profile/index.jsx
+++ src/pages/Profile/index.jsx
@@ -1,13 +1,15 @@
 import { useState } from "react";
 import { FiArrowLeft, FiUser, FiMail, FiLock, FiCamera } from "react-icons/fi";
-import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 import { useAuth } from "../../hooks/auth";
 
 import { api } from "../../services/api";
-import avatarPlaceholder from "../../assets/avatar_placeholder.svg"
+
 import { Input } from "../../components/Input";
 import { Button } from "../../components/Button";
+
+import avatarPlaceholder from "../../assets/avatar_placeholder.svg";
 
 import { Container, Form, Avatar } from "./styles";
 
@@ -19,10 +21,18 @@ export function Profile() {
   const [passwordOld, setPasswordOld] = useState();
   const [passwordNew, setPasswordNew] = useState();
 
-  const avatarUrl = user.avatar ? `${api.defaults.baseURL}/files/${user.avatar}` : avatarPlaceholder;
+  const avatarUrl = user.avatar
+    ? `${api.defaults.baseURL}/files/${user.avatar}`
+    : avatarPlaceholder;
 
   const [avatar, setAvatar] = useState(avatarUrl);
   const [avatarFile, setAvatarFile] = useState(null);
+
+  const navigate = useNavigate();
+
+  function handleBack() {
+    navigate(-1);
+  }
 
   async function handleUpdate() {
     const user = {
@@ -46,9 +56,9 @@ export function Profile() {
   return (
     <Container>
       <header>
-        <Link to="/">
+        <button type="button" onClick={handleBack}>
           <FiArrowLeft />
-        </Link>
+        </button>
       </header>
 
       <Form>
