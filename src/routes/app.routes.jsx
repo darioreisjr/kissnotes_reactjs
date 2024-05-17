1eb65a29f819aa0185730d5ab360fbd234c6d4c1
Dario Reis <guttyerresjunior1995@gmail.com>
2024-05-17 05:10:30 -0300

    Feat: Handling Unavailable Routes

--- src/routes/app.routes.jsx
+++ src/routes/app.routes.jsx
@@ -1,4 +1,4 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 
 import { New } from "../pages/New";
 import { Home } from "../pages/Home";
@@ -10,8 +10,10 @@ export function AppRoutes() {
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/new" element={<New />} />
-      <Route path="/details/:id" element={<Details />} />
       <Route path="/profile" element={<Profile />} />
+      <Route path="/details/:id" element={<Details />} />
+
+      <Route path="*" element={<Navigate to="/" />} />
     </Routes>
   );
 }
